188163824e222d53c6b7f17af10378b96c3b2b8c
Shiva <sh.ramezani94@gmail.com>
2021-05-09 20:26:52 +0430

    flask-updated global variables

--- Code/flask-restapi.py
+++ Code/flask-restapi.py
@@ -1,32 +1,32 @@
 # importing the required libraries
+import settings
 from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
 from tensorflow.keras.models import load_model
-MODEL_DIR = '../Models/' 
-SEQUENCE_LEN=128
+
 # load the pipeline object
-model = load_model(MODEL_DIR+'temp_model.h5')
-with open('../Models/tokenizer.p', 'rb') as handle:
+model = load_model(settings.MODEL_PATH)
+
+with open(settings.TOKENIZER_PATH, 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-def prepare_data(text):
-    print("=================================")
-    
+def prepare_data(text):    
     sequence_docs = tokenizer.texts_to_sequences([text])
     # pad data to Maximum Sequence Length
-    pad_sequence_docs = pad_sequences(sequence_docs, maxlen=SEQUENCE_LEN, padding='post')
+    pad_sequence_docs = pad_sequences(sequence_docs, maxlen=settings.SEQUENCE_LEN, padding='post')
     return pad_sequence_docs
  # function to get results for a particular text query
 def requestResults(text):
-    padded_text = prepare_data(text )
-    
-    prediction = model.predict(padded_text)
-    pred = np.argmax(prediction, axis = 1)
-    data = "result: "+str(pred) + '\n\n'
-    return data + "text: "+str(text)+'\n\n'
+    try:
+        padded_text = prepare_data(text )
+        prediction = model.predict(padded_text)
+        pred = np.argmax(prediction, axis = 1)
+        data = "Sentiment: "+str(settings.LABEL_DIC[pred[0]]) + '\n\n'
+        return "text: "+str(text)+'\n\n'+ data  
+    except Exception :
+        return "Systemt couldn't predict sentiment of your text\n please enter a valid text"
 
 
 
