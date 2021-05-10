fc85173b563f428f04ca5e1974a1efb76d759f76
Shiva <sh.ramezani94@gmail.com>
2021-05-10 07:52:06 +0430

    settings- updated embeding path

--- Code/settings.py
+++ Code/settings.py
@@ -1,13 +1,19 @@
 
 #Declare global variables
+SEED =1234
+EMB_SIZE =100
+SEQUENCE_LEN = 128
+CLASS_NUM = 5
 
 MODEL_DIR = '../Models/' 
+DATA_DIR  = '../Dataset/'
+EMB_DIR   = '../Embedding/'
+
 MODEL_PATH = MODEL_DIR+'temp_model.h5'
-DATA_PATH = '../Dataset/'
 TOKENIZER_PATH = MODEL_DIR+'tokenizer.p'
-SEED =1234
-EMB_SIZE =300
-SEQUENCE_LEN = 128
+ORIG_DATA_PATH = DATA_DIR+'DeepSentiPers-original.csv'
+EMBEDDING_PATH = EMB_DIR+'fasttext-{emb_size}.bin'.format(emb_size = str(EMB_SIZE))
+
 
 
 LABEL_DIC={0:'Furious',
